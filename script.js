73914f7171cc6eb131fff3595cd2f06d8bcd0aa9
Rida F'kih <ridafakih@gmail.com>
2022-03-04 01:12:03 -0700

    add jsdocs, because why not?

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+/**
+ * Resolves a promise after a set amount of time.
+ * @param {number} time The number of milliseconds to wait.
+ * @returns {Promise<void>} A promise that resolves after the set time.
+ */
 const wait = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
+/**
+ * Flips a flipper to reveal a new value.
+ * @param {HTMLDivElement} flipper The flipper element to flip.
+ * @param {string|number} flipperTextValue The new flipper value.
+ * @returns {Promise<void>} A promise that resolves on completion.
+ */
 const flipFlipper = async (flipper, flipperTextValue) => {
   const [base, flipped] = flipper.children;
   flipped.textContent = flipperTextValue;
@@ -11,9 +22,14 @@ const flipFlipper = async (flipper, flipperTextValue) => {
   void flipper.offsetWidth;
 };
 
-const flipAllFlippers = async (newNumber) => {
+/**
+ * Flips all the flippers on the scoreboard to reveal a new value.
+ * @param {string|number} newNumber The new value for the entire scoreboard.
+ * @returns {Promise<void>} A promise that resolves on completion.
+ */
+const flipAllFlippers = async (newValue) => {
   const flippers = [...document.querySelectorAll(".flippy")];
-  const chars = [...newNumber.toString().padStart(4, "0")];
+  const chars = [...newValue.toString().padStart(4, "0")];
 
   const flips = flippers.map((flipper, index) => {
     const { textContent: currentChar } = flipper.children[0];
@@ -24,7 +40,12 @@ const flipAllFlippers = async (newNumber) => {
   return Promise.all(flips);
 };
 
-const incrementNumberRecursively = async (value = 0) => {
+/**
+ * Recursively calls itself at a random interval to continuously
+ * add a random value to the scoreboard.
+ * @param {string|value} value The value to set the scoreboard to.
+ */
+(async function incrementNumberRecursively(value = 0) {
   await flipAllFlippers(value);
   value += Math.floor(Math.random() * 9) + 1;
 
@@ -32,6 +53,4 @@ const incrementNumberRecursively = async (value = 0) => {
   const nextValue = value <= 9999 ? value : value - 9999;
 
   setTimeout(incrementNumberRecursively, timeout, nextValue);
-};
-
-incrementNumberRecursively();
+})();
